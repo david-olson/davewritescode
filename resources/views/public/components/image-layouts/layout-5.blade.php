baf9734fbd484cb5952ccbe122b2bf76377e1a92
David Olson <dave@blueion.com>
2020-09-16 09:34:58 -0400

    Add updates to image layouts.

--- resources/views/public/components/image-layouts/layout-5.blade.php
+++ resources/views/public/components/image-layouts/layout-5.blade.php
@@ -1,8 +1,8 @@
 <div class="image-layout-scene image-layout-scene--5">
-	<div class="image-layout-scene__image-10">
+	{{-- <div class="image-layout-scene__image-10">
 		<img src="http://placehold.it/400x900" alt="">
 		<div class="below-3"></div>
-	</div>
+	</div> --}}
 	<div class="image-layout-scene__group image-layout-scene__group--3">
 		<div class="image-layout-scene__image-11">
 			<img src="http://placehold.it/600x400" alt="">
