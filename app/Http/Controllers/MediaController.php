9366e1a1b6e2d9746b0d1a5800235c8bcd7eb59f
David Olson <dave@blueion.com>
2020-11-05 12:55:21 -0500

    Adds updates to remove unused methods from MediaController

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -10,25 +10,6 @@ use Storage;
 
 class MediaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -123,14 +104,4 @@ class MediaController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Media  $media
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Media $media)
-    {
-        //
-    }
 }
